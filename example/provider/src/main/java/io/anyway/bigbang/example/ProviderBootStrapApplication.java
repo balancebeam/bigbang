8d7b6bf0e3bdef1774471ca402b199c2d665a211
balancebeam <balancebeam@163.com>
2020-10-26 23:44:13 +0800

    modify dependency

--- example/provider/src/main/java/io/anyway/bigbang/example/ProviderBootStrapApplication.java
+++ example/provider/src/main/java/io/anyway/bigbang/example/ProviderBootStrapApplication.java
@@ -1,7 +1,6 @@
 package io.anyway.bigbang.example;
 
 import io.anyway.bigbang.framework.bootstrap.SpringApplicationBootStrap;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
