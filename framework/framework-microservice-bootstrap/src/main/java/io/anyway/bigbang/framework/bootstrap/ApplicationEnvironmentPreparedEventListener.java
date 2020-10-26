a5f4ba1f7bbb03ef0f7ea9c9cc182e4c47bdce59
zz <zz@zzdeMacBook-Pro.local>
2020-10-26 15:08:30 +0800

    add log

--- framework/framework-microservice-bootstrap/src/main/java/io/anyway/bigbang/framework/bootstrap/ApplicationEnvironmentPreparedEventListener.java
+++ framework/framework-microservice-bootstrap/src/main/java/io/anyway/bigbang/framework/bootstrap/ApplicationEnvironmentPreparedEventListener.java
@@ -7,6 +7,9 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.MutablePropertySources;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,7 +25,24 @@ public class ApplicationEnvironmentPreparedEventListener implements ApplicationL
         map.put("spring.cloud.kubernetes.discovery.enabled","false");
         map.put("spring.cloud.kubernetes.loadbalancer.enabled","false");
         map.put("spring.cloud.nacos.discovery.enabled","false");
+        map.put("logging.config","classpath:logging/logback-text-console.xml");
+        map.put("spring.sleuth.enabled","false")
         //TODO find default properties at each component classpath*:META-INF/default.properties
         source.addLast(new MapPropertySource("module-default-properties",map));
+    }
+
+    private void f(){
+        PathMatchingResourcePatternResolver pathMatchingResourcePatternResolver = new PathMatchingResourcePatternResolver();
+        Properties properties = new Properties();
+        try {
+            Resource[] resources = pathMatchingResourcePatternResolver.getResources("classpath*:/META-INF/default.properties");
+            for (Resource each : resources) {
+                try (InputStream in = each.getInputStream()) {
+                    properties.load(in);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
