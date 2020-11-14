51dec6c66cecb59ab276fd9985047d0f9b157b9b
balancebeam <balancebeam@163.com>
2020-11-14 19:02:44 +0800

    modify k8s gray

--- basicservice-gateway/src/main/java/io/anyway/bigbang/gateway/gray/KubernetesGrayRibbonRuleImpl.java
+++ basicservice-gateway/src/main/java/io/anyway/bigbang/gateway/gray/KubernetesGrayRibbonRuleImpl.java
@@ -1,6 +1,7 @@
 package io.anyway.bigbang.gateway.gray;
 
 import io.anyway.bigbang.framework.gray.GrayContext;
+import io.fabric8.kubernetes.api.model.EndpointAddress;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
@@ -8,39 +9,52 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.reactive.DefaultResponse;
 import org.springframework.cloud.client.loadbalancer.reactive.EmptyResponse;
 import org.springframework.cloud.client.loadbalancer.reactive.Response;
+import org.springframework.cloud.kubernetes.discovery.KubernetesServiceInstance;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.util.StringUtils;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class KubernetesGrayRibbonRuleImpl implements GrayRibbonRule{
 
     private AtomicInteger pos= new AtomicInteger(new Random().nextInt(1000));
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
+    final private Field f;
+
+    public KubernetesGrayRibbonRuleImpl(){
+        f= ReflectionUtils.findField(KubernetesServiceInstance.class,"endpointAddress");
+        ReflectionUtils.makeAccessible(f);
+    }
 
     @Override
     public Response<ServiceInstance> choose(String serviceId, List<ServiceInstance> instances, GrayContext ctx){
-        List<ServiceInstance> availableInstances= discoveryClient.getInstances(getK8sServiceName(serviceId,ctx.getGroup()));
-        if(CollectionUtils.isEmpty(availableInstances)){
-            availableInstances= discoveryClient.getInstances(getK8sServiceName(serviceId,ctx.getDefGroup()));
+        log.debug("kubernetes service {} instances: {}",serviceId,instances);
+        final String serviceName= serviceId+"-"+ctx.getGroup();
+        List<ServiceInstance> availableInstances= instances.stream().filter(each-> {
+            EndpointAddress endpointAddress= (EndpointAddress)ReflectionUtils.getField(f,each);
+            return endpointAddress.getTargetRef().getName().startsWith(serviceName);
+        }).collect(Collectors.toList());
+        //lookup the default group and cluster
+        if(availableInstances.isEmpty() &&
+                ctx.getDefGroup()!= null &&
+                !ctx.getGroup().equals(ctx.getDefGroup())){
+            final String serviceName2= serviceId+"-"+ctx.getDefGroup();
+            availableInstances= instances.stream().filter(each-> {
+                EndpointAddress endpointAddress= (EndpointAddress)ReflectionUtils.getField(f,each);
+                return endpointAddress.getTargetRef().getName().startsWith(serviceName2);
+            }).collect(Collectors.toList());
         }
-        if(!CollectionUtils.isEmpty(availableInstances)){
+        if(!availableInstances.isEmpty()){
             ServiceInstance instance= availableInstances.get(Math.abs(pos.incrementAndGet()) % availableInstances.size());
             return new DefaultResponse(instance);
         }
         log.warn("cannot find appropriate match candidate server: {}",ctx.toString());
         return new EmptyResponse();
-    }
-
-    private String getK8sServiceName(String serviceId,String group){
-        if(StringUtils.isEmpty(group)){
-            return serviceId;
-        }
-        return serviceId+ "-"+group;
     }
 }
--- framework/framework-microservice-gray/src/main/java/io/anyway/bigbang/framework/gray/K8SGrayRibbonRule.java
+++ framework/framework-microservice-gray/src/main/java/io/anyway/bigbang/framework/gray/K8SGrayRibbonRule.java
@@ -5,18 +5,28 @@ import com.netflix.loadbalancer.BaseLoadBalancer;
 import com.netflix.loadbalancer.ILoadBalancer;
 import com.netflix.loadbalancer.Server;
 import com.netflix.loadbalancer.ZoneAvoidanceRule;
+import io.fabric8.kubernetes.api.model.EndpointAddress;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.reactive.DefaultResponse;
+import org.springframework.cloud.client.loadbalancer.reactive.EmptyResponse;
+import org.springframework.cloud.kubernetes.discovery.KubernetesServiceInstance;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.util.StringUtils;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class K8SGrayRibbonRule extends ZoneAvoidanceRule {
+
+    private AtomicInteger pos= new AtomicInteger(new Random().nextInt(1000));
 
     private String serviceId;
 
@@ -24,6 +34,13 @@ public class K8SGrayRibbonRule extends ZoneAvoidanceRule {
     private DiscoveryClient discoveryClient;
 
     private Random random= new Random();
+
+    private static Field f;
+
+    static {
+        f= ReflectionUtils.findField(KubernetesServiceInstance.class,"endpointAddress");
+        ReflectionUtils.makeAccessible(f);
+    }
 
     @Override
     public void setLoadBalancer(ILoadBalancer lb) {
@@ -46,26 +63,33 @@ public class K8SGrayRibbonRule extends ZoneAvoidanceRule {
                 return null;
             }
         }
+
         GrayContext ctx=grayContext.get();
-        List<ServiceInstance> instances= discoveryClient.getInstances(getK8sServiceName(ctx.getGroup()));
+        List<ServiceInstance> instances= discoveryClient.getInstances(serviceId);
+        log.debug("kubernetes service {} instances: {}",serviceId,instances);
         if(CollectionUtils.isEmpty(instances)){
-            instances= discoveryClient.getInstances(getK8sServiceName(ctx.getDefGroup()));
-        }
-        if(!CollectionUtils.isEmpty(instances)){
-            int index= random.nextInt(instances.size());
-            ServiceInstance instance= instances.get(index);
-            return new Server(instance.getHost(),instance.getPort());
-        }
-        else{
             return null;
         }
-    }
-
-
-    private String getK8sServiceName(String group){
-        if(StringUtils.isEmpty(group)){
-            return serviceId;
+        final String serviceName= serviceId+"-"+ctx.getGroup();
+        List<ServiceInstance> availableInstances= instances.stream().filter(each-> {
+            EndpointAddress endpointAddress= (EndpointAddress)ReflectionUtils.getField(f,each);
+            return endpointAddress.getTargetRef().getName().startsWith(serviceName);
+        }).collect(Collectors.toList());
+        //lookup the default group and cluster
+        if(availableInstances.isEmpty() &&
+                ctx.getDefGroup()!= null &&
+                !ctx.getGroup().equals(ctx.getDefGroup())){
+            final String serviceName2= serviceId+"-"+ctx.getDefGroup();
+            availableInstances= instances.stream().filter(each-> {
+                EndpointAddress endpointAddress= (EndpointAddress)ReflectionUtils.getField(f,each);
+                return endpointAddress.getTargetRef().getName().startsWith(serviceName2);
+            }).collect(Collectors.toList());
         }
-        return serviceId+ "-"+group;
+        if(!availableInstances.isEmpty()){
+            ServiceInstance instance= availableInstances.get(Math.abs(pos.incrementAndGet()) % availableInstances.size());
+            return new Server(instance.getHost(),instance.getPort());
+        }
+        log.warn("cannot find appropriate match candidate server: {}",ctx.toString());
+        return null;
     }
 }
