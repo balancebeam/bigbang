fb320034f5d6dd96b4f04fc04b4141e456c611cd
myubuntu <balancebeam@163.com>
2021-02-24 17:53:51 -0800

    modify oauth

--- oauth2/oauth2-service/src/main/java/io/anyway/bigbang/oauth2/config/WebSecurityConfig.java
+++ oauth2/oauth2-service/src/main/java/io/anyway/bigbang/oauth2/config/WebSecurityConfig.java
@@ -9,12 +9,17 @@ import org.apache.http.conn.ssl.TrustAllStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,13 +33,17 @@ import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import javax.annotation.Resource;
 import java.security.KeyPair;
 import java.util.HashMap;
 import java.util.Locale;
@@ -44,7 +53,11 @@ import java.util.Map;
 @Slf4j
 @Configuration
 @EnableWebSecurity
+@AutoConfigureBefore({WebSecurityConfig.JwtTokenConfig.class, WebSecurityConfig.XDefaultTokenConfig.class})
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Resource
+    private RedisConnectionFactory redisConnectionFactory;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -56,16 +69,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new XUserDetailsServiceImpl();
     }
 
-
-
     @Bean
-    public TokenEnhancer createTokenEnhancer() {
-        return (accessToken, authentication) -> {
-            if (authentication.getPrincipal() instanceof XUserDetails) {
-                enhanceAccessToken(accessToken, authentication);
-            }
-            return accessToken;
-        };
+    public TokenStore createDefaultTokenStore() {
+        return new RedisTokenStore(redisConnectionFactory);
     }
 
     private static void enhanceAccessToken(OAuth2AccessToken accessToken,OAuth2Authentication authentication){
@@ -117,7 +123,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/**").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable();
-        ;
     }
 
     @Override
@@ -133,8 +138,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Configuration
-    @ConditionalOnProperty(prefix= "spring.security.oauth2.jwt", name = "enabled",havingValue="true" ,matchIfMissing = false)
-    public static class JwtConfig{
+    @ConditionalOnProperty(prefix= "spring.security.oauth2.jwt", name = "enabled",havingValue="true")
+    public static class JwtTokenConfig{
         @Value("${spring.security.oauth2.jwt.keystore-path:jwt-cert.jks}")
         private String certPath;
 
@@ -147,13 +152,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         @Value("${spring.security.oauth2.jwt.keypair-password:keypairpass}")
         private String keypairPwd;
 
+        @Resource
+        private XUserDetailsServiceImpl userDetailsService;
+
+        @Resource
+        private AuthenticationManager authenticationManager;
+
+        @Resource
+        private TokenStore tokenStore;
+
         @Bean
         public KeyPair keyPair() {
             KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource(certPath), keystorePwd.toCharArray());
             return keyStoreKeyFactory.getKeyPair(keypairAlias, keypairPwd.toCharArray());
         }
 
-        @Bean
+        @Bean("jwtAccessTokenConverter")
         public AccessTokenConverter createJwtAccessTokenConverter(KeyPair keyPair) {
             JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter(){
                 @Override
@@ -164,6 +178,60 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             };
             jwtAccessTokenConverter.setKeyPair(keyPair);
             return jwtAccessTokenConverter;
+        }
+
+        @Bean
+        public XAuthorizationServerConfigurer.TokenConfigure createJwtTokenConfigure(
+                @Qualifier("jwtAccessTokenConverter") AccessTokenConverter accessTokenConverter){
+            return endpoints -> endpoints
+                    .tokenStore(tokenStore)
+                    .authenticationManager(authenticationManager)
+                    .userDetailsService(userDetailsService)
+                    .accessTokenConverter(accessTokenConverter);
+        }
+    }
+
+    @Configuration
+    public static class XDefaultTokenConfig {
+
+        @Resource
+        private TokenStore tokenStore;
+
+        @Resource
+        private XUserDetailsServiceImpl userDetailsService;
+
+        @Resource
+        private AuthenticationManager authenticationManager;
+
+        @Bean
+        @ConditionalOnMissingBean
+        public XAuthorizationServerConfigurer.TokenConfigure createDefaultTokenConfigure(){
+            TokenEnhancer tokenEnhancer= createDefaultTokenEnhancer();
+            return endpoints -> endpoints
+                    .tokenServices(defaultTokenServices(tokenEnhancer))
+                    .tokenStore(tokenStore)
+                    .authenticationManager(authenticationManager)
+                    .userDetailsService(userDetailsService)
+                    .tokenEnhancer(tokenEnhancer);
+        }
+
+        private TokenEnhancer createDefaultTokenEnhancer() {
+            return (accessToken, authentication) -> {
+                if (authentication.getPrincipal() instanceof XUserDetails) {
+                    enhanceAccessToken(accessToken, authentication);
+                }
+                return accessToken;
+            };
+        }
+
+        private DefaultTokenServices defaultTokenServices(TokenEnhancer tokenEnhancer) {
+            DefaultTokenServices tokenServices = new DefaultTokenServices();
+            tokenServices.setTokenStore(tokenStore);
+            tokenServices.setSupportRefreshToken(true);
+            tokenServices.setAccessTokenValiditySeconds(60 * 60 * 24 * 7);
+            tokenServices.setRefreshTokenValiditySeconds(60 * 60 * 24 * 7);
+            tokenServices.setTokenEnhancer(tokenEnhancer);
+            return tokenServices;
         }
     }
 }
--- oauth2/oauth2-service/src/main/java/io/anyway/bigbang/oauth2/config/XAuthorizationServerConfigurer.java
+++ oauth2/oauth2-service/src/main/java/io/anyway/bigbang/oauth2/config/XAuthorizationServerConfigurer.java
@@ -1,23 +1,12 @@
 package io.anyway.bigbang.oauth2.config;
 
-import io.anyway.bigbang.oauth2.service.XUserDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
@@ -31,71 +20,32 @@ public class XAuthorizationServerConfigurer extends AuthorizationServerConfigure
     private DataSource dataSource;
 
     @Resource
-    private RedisConnectionFactory redisConnectionFactory;
-
-    @Resource
-    private AuthenticationManager authenticationManager;
-
-    @Resource
     private PasswordEncoder passwordEncoder;
 
     @Resource
-    private TokenEnhancer tokenEnhancer;
-
-    @Resource
-    private XUserDetailsServiceImpl userDetailsService;
-
-    @Autowired(required = false)
-    private AccessTokenConverter accessTokenConverter;
+    private TokenConfigure tokenConfigure;
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.allowFormAuthenticationForClients()
                 .passwordEncoder(passwordEncoder)
                 .tokenKeyAccess("permitAll()")
-                .checkTokenAccess("isAuthenticated()")
-        ;
+                .checkTokenAccess("isAuthenticated()");
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-//        super.configure(clients);
         clients.jdbc(dataSource)
-                .passwordEncoder(passwordEncoder)
-        ;
+                .passwordEncoder(passwordEncoder);
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        if(accessTokenConverter!= null) {
-            endpoints
-                    .tokenStore(defaultTokenStore())
-                    .authenticationManager(authenticationManager)
-                    .userDetailsService(userDetailsService)
-                    .accessTokenConverter(accessTokenConverter);
-        }
-        else{
-            endpoints.tokenServices(defaultTokenServices())
-                    .tokenStore(defaultTokenStore())
-                    .authenticationManager(authenticationManager)
-                    .userDetailsService(userDetailsService)
-                    .tokenEnhancer(tokenEnhancer);
-        }
+        tokenConfigure.configure(endpoints);
     }
 
-
-    private DefaultTokenServices defaultTokenServices() {
-        DefaultTokenServices tokenServices = new DefaultTokenServices();
-        tokenServices.setTokenStore(defaultTokenStore());
-        tokenServices.setSupportRefreshToken(true);
-        tokenServices.setAccessTokenValiditySeconds(60 * 60 * 24 * 7);
-        tokenServices.setRefreshTokenValiditySeconds(60 * 60 * 24 * 7);
-        tokenServices.setTokenEnhancer(tokenEnhancer);
-        return tokenServices;
-    }
-
-    private TokenStore defaultTokenStore() {
-        return new RedisTokenStore(redisConnectionFactory);
+    public interface TokenConfigure{
+        void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception;
     }
 
 }
