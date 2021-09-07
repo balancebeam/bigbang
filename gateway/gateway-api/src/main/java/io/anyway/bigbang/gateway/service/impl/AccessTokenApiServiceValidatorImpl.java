9c973ef3a2f68f1a6fd5ea6da7984c8aadb79c8b
balancebeam <balancebeam@163.com>
2021-09-07 23:25:57 +0800

    rename func

--- gateway/gateway-api/src/main/java/io/anyway/bigbang/gateway/service/impl/AccessTokenApiServiceValidatorImpl.java
+++ gateway/gateway-api/src/main/java/io/anyway/bigbang/gateway/service/impl/AccessTokenApiServiceValidatorImpl.java
@@ -1,17 +1,13 @@
 package io.anyway.bigbang.gateway.service.impl;
 
+import io.anyway.bigbang.framework.session.DefaultUserDetailContext;
 import io.anyway.bigbang.framework.session.UserDetailContext;
-import io.anyway.bigbang.framework.utils.JsonUtil;
 import io.anyway.bigbang.gateway.service.AccessTokenValidator;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 
-import javax.annotation.Resource;
 import java.util.Optional;
 
 @Slf4j
@@ -19,58 +15,14 @@ import java.util.Optional;
 @ConditionalOnProperty(prefix = "spring.cloud.gateway.token-validator",name = "mode",havingValue = "api")
 public class AccessTokenApiServiceValidatorImpl implements AccessTokenValidator {
 
-    private String WEB_ACCESS_KEY_CACHE_KEY = "industry:web:user:cache:%s";
-
-    @Resource
-    private RedisTemplate redisTemplate;
-
     @Override
     public Optional<UserDetailContext> check(ServerWebExchange exchange) {
-        String accessKey= exchange.getRequest().getHeaders().getFirst("accessKey");
-        if(StringUtils.isEmpty(accessKey)) {
-            return Optional.empty();
-        }
-        String redisKey = String.format(WEB_ACCESS_KEY_CACHE_KEY, accessKey);
-        String cacheStr =(String)redisTemplate.opsForValue().get(redisKey);
-        if(StringUtils.isEmpty(cacheStr)){
-            return Optional.empty();
-        }
-        return Optional.of(JsonUtil.fromString2Object(cacheStr,WebUserBO.class));
+        DefaultUserDetailContext ctx= new DefaultUserDetailContext();
+        ctx.setUserId("1");
+        ctx.setAppId("app");
+        ctx.setUserName("Tom");
+        return Optional.of(ctx);
     }
 
-    @Data
-    public static class WebUserBO implements UserDetailContext {
-        private String userId;
 
-        private String nickName;
-
-        private String wxOpenId;
-
-        /**
-         * 用户头像
-         */
-        private String avatarUrl;
-
-        private String phoneNum;
-
-        private Integer userType;
-
-        /**
-         * 用户类型描述
-         */
-        private String userTypeDesc;
-
-        private Integer staffType;
-
-        private Integer department;
-
-        private String accessKey;
-
-        private String sessionKey;
-
-        @Override
-        public String getUserName() {
-            return nickName;
-        }
-    }
 }
