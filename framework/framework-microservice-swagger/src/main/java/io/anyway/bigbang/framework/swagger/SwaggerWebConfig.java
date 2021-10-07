f56d265b1c9c0291edcf4f423a55c275d4b88c7a
balancebeam <balancebeam@163.com>
2021-10-07 21:49:09 +0800

    modify swagger

--- framework/framework-microservice-swagger/src/main/java/io/anyway/bigbang/framework/swagger/SwaggerWebConfig.java
+++ framework/framework-microservice-swagger/src/main/java/io/anyway/bigbang/framework/swagger/SwaggerWebConfig.java
@@ -29,8 +29,9 @@ public class SwaggerWebConfig implements WebMvcConfigurer {
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
+//        registry.addResourceHandler("swagger-ui.html")
+//            registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("doc.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
--- framework/framework-microservice-swagger/src/main/java/io/anyway/bigbang/framework/swagger/config/Swagger2Configure.java
+++ framework/framework-microservice-swagger/src/main/java/io/anyway/bigbang/framework/swagger/config/Swagger2Configure.java
@@ -4,18 +4,24 @@ import io.anyway.bigbang.framework.swagger.SwaggerWebConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Slf4j
@@ -25,48 +31,20 @@ import javax.servlet.http.HttpServletRequest;
 @ConditionalOnClass(HttpServletRequest.class)
 public class Swagger2Configure {
 
-//    @Resource
-//    private BeanFactory beanFactory;
-//
-//    @Resource
-//    private ResourceLoader resourceLoader;
-
     @Bean
+    @ConditionalOnMissingBean
     public Docket createRestApi() {
-//        List<String> packages= AutoConfigurationPackages.get(this.beanFactory);
-//        if(packages.isEmpty()){
-//            for(String each: ((DefaultListableBeanFactory)beanFactory).getBeanDefinitionNames()) {
-//                try {
-//                    BeanDefinition beanDefinition = ((DefaultListableBeanFactory) beanFactory).getBeanDefinition(each);
-//                    String beanName = beanDefinition.getBeanClassName();
-//                    Class<?> cls = resourceLoader.getClassLoader().loadClass(beanName);
-//                    if (cls.getAnnotation(SpringBootApplication.class) != null) {
-//                        ComponentScan componentScan = cls.getAnnotation(ComponentScan.class);
-//                        if (componentScan != null) {
-//                            packages.addAll(Arrays.asList(componentScan.value()));
-//                        }
-//                        ComponentScans componentScans = cls.getAnnotation(ComponentScans.class);
-//                        if (componentScans != null) {
-//                            for (ComponentScan c : componentScans.value()) {
-//                                packages.addAll(Arrays.asList(c.value()));
-//                            }
-//                        }
-//                        break;
-//                    }
-//                } catch (ClassNotFoundException e) {
-//                    log.debug("class not found.", e);
-//                }
-//            }
-//        }
-//        if(packages.isEmpty()){
-//            throw new RuntimeException("package was empty.");
-//        }
-
         return new Docket(DocumentationType.SWAGGER_2)
                 .pathMapping("/")
+                .globalOperationParameters(setHeaderToken())
+                .pathProvider(new RelativePathProvider(null) {
+                    @Override
+                    public String getApplicationBasePath() {
+                        return "";
+                    }
+                })
                 .apiInfo(apiInfo())
                 .select()
-//                .apis(RequestHandlerSelectors.basePackage(packages.get(0)))
                 .apis(RequestHandlerSelectors.withClassAnnotation(RequestMapping.class))
                 .paths(PathSelectors.any())
                 .build();
@@ -83,6 +61,12 @@ public class Swagger2Configure {
                 .build();
     }
 
-
+    private List<Parameter> setHeaderToken() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        tokenPar.name("accessKey").description("用户登录鉴权").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        List<Parameter> pars = new ArrayList<>();
+        pars.add(tokenPar.build());
+        return pars;
+    }
 
 }
