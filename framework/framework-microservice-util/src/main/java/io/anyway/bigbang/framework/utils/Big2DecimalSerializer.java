3e785cf673433ffe3ae2203722a76c37c4a86aa5
yangzuozhong <zuozhong_yang>
2022-03-28 15:30:22 +0800

    add bigdecimalserializer

---
+++ framework/framework-microservice-util/src/main/java/io/anyway/bigbang/framework/utils/Big2DecimalSerializer.java
@@ -0,0 +1,21 @@
+package io.anyway.bigbang.framework.utils;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
+public class Big2DecimalSerializer extends JsonSerializer<BigDecimal> {
+    @Override
+    public void serialize(BigDecimal value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        if (Objects.isNull(value)) {
+            gen.writeNull();
+        } else {
+            gen.writeNumber(value.setScale(2, RoundingMode.HALF_UP));
+        }
+    }
+}
